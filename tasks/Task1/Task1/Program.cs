d01322f73d86c27e2ea6fc84688d6aaaf19efecf
Oguz <kayaouz@hotmail.com>
2016-04-25 23:17:09 +0200

    add project Task2

--- tasks/Task1/Task1/Program.cs
+++ tasks/Task1/Task1/Program.cs
@@ -7,6 +7,7 @@ namespace Task1
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Hello World!");
+		
 		}
 	}
 }
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+
+namespace Task2
+{
+	public class Ball
+	{
+		private decimal price;
+
+		
+		public Ball(string ballName, decimal ballID, decimal price  )
+		{
+			Console.WriteLine ("Hello World!");
+		}
+
+	}
+}
